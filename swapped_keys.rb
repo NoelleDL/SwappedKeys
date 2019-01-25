351f90992987d8d9ab329e13befd4ed132f8051a
NoelleDL <noelle.legrain@gmail.com>
2019-01-25 10:49:07 +0000

    method returns hash with key value pairs

--- swapped_keys.rb
+++ swapped_keys.rb
@@ -1,15 +1,20 @@
 class TechTest
 
   expressions_array = [
-    '123 = 3',
-    '8423 = 252',
-    '4+4 = 8',
-    '4*7-10 = 417',
-    '9/3 = 3',
-    '42-9 = -36'
+    '123=3',
+    '8423=252',
+    '4+4=8',
+    '4*7-10=417',
+    '9/3=3',
+    '42-9=-36'
   ]
 
   def array_to_hash(expressions_array)
-  end 
-
+    expressions_hash = Hash.new()
+    expressions_array.each do |expression|
+      ex = expression.split('=')
+      expressions_hash[ex[0]] = ex[1].to_f
+    end
+    expressions_hash
+  end
 end
