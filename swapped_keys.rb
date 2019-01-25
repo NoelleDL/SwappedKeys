0ea26dd3121223b163cac15290658a3c28cedb43
NoelleDL <noelle.legrain@gmail.com>
2019-01-25 11:09:07 +0000

    refactor array so that expressions are exactly the same as given in task

--- swapped_keys.rb
+++ swapped_keys.rb
@@ -1,19 +1,19 @@
 class SwappedKeys
 
   expressions_array = [
-    '123=3',
-    '8423=252',
-    '4+4=8',
-    '4*7-10=417',
-    '9/3=3',
-    '42-9=-36'
+    '123 = 3',
+    '8423 = 252',
+    '4+4 = 8',
+    '4*7-10 = 417',
+    '9/3 = 3',
+    '42-9 = -36'
   ]
 
   def array_to_hash(expressions_array)
     expressions_hash = Hash.new()
     expressions_array.each do |expression|
       ex = expression.split('=')
-      expressions_hash[ex[0]] = ex[1].to_f
+      expressions_hash[ex[0].strip] = ex[1].strip.to_f
     end
     expressions_hash
   end
